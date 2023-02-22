25b94c9db3602d8aeec8cbc82db8f5f7b54f59cd
Felix Rodriguez <108102495+ItsFelixRodriguez@users.noreply.github.com>
2023-02-22 17:12:31 +0900

    currently done but would be fun to add a HTML element to this.

--- README.md
+++ README.md
@@ -5,12 +5,8 @@ Codecademy Portfolio Project
 This project deploys my skills in Javascript, command line, and git to generate a series of random messages to output when a user runs the script. 
 
 ### Goals/To-Do
-+ Generate an array of pieces of "terrible advice" to return to user when script is run based on a horoscope.
-+ Generate an array of astrological signs.
-+ Generate an array of fortune variables. 
-+ Create a random number generation function that can operate for all variable aspects of the horoscope.
-+ Write a function that uses **SWITCH/CASE** to swap out astrological signs. 
-+ Write a function that uses **SWITCH/CASE** to swap out advice.
++ The Project currently works in Node. 
++ It returns a random Sign, Luck Level, and Piece of Advice. 
 
 ### Not Stricly Necessary but would be nice to add. 
 + Write an HTML page to display the result.
--- script.js
+++ script.js
@@ -4,8 +4,8 @@
 let mixed = {
 
 	sign : ['Aries','Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capicorn', 'Aquarius', 'Pisces'],
-	luck : ['Terrible', 'God Awful', 'Unspeakably Bleak', 'Non-existent', 'Nothing to write home about', '... better not to say', '?!?!?!?!?!?!', 'okayish', 'Actually not that bad', 'Gegrudgingly good', ';_;', 'High'],
-	advice : ['Run!!!', 'Quit your job', "Don't look behind your sholder.", 'Start a podcast.', 'Definitely buy that thing.', 'Put it all on black.', 'Steal for fun.', 'Change your name.', 'Build an alliance', 'Forget?', 'Open your windows.', 'Cancel yourself.']
+	luck : ['Terrible', 'God Awful', 'Unspeakably Bleak', 'Non-existent', 'Nothing to write home about', '... better not to say', '?!?!?!?!?!?!', 'Okayish', 'Actually not that bad', 'Gegrudgingly good', ';_;', 'High'],
+	advice : ['Run!!!', 'Quit your job', "Don't look behind your shoulder.", 'Start a podcast.', 'Definitely buy that thing.', 'Put it all on black.', 'Steal for fun.', 'Change your name.', 'Build an alliance', 'Forget?', 'Open your windows.', 'Cancel yourself.']
 }
 // console.log (mixed)
 
@@ -22,28 +22,19 @@ function getRandomItem(arr) {
 	return item;
 }
 
-const randSign = getRandomItem(mixed.sign)
-const randLuck = getRandomItem(mixed.luck)
-const randAdvice = getRandomItem(mixed.advice)
-
-// Testing the function on each item of the Object
-console.log(randSign);
-console.log(randLuck);
-console.log(randAdvice);
-
 // Constructing the Horoscope
 
-const horoscope = (randSign, randLuck, randAdvice) => {
+const horoscope = () => {
+	let randSign = getRandomItem(mixed.sign);
+	let randLuck = getRandomItem(mixed.luck);
+	let randAdvice = getRandomItem(mixed.advice);
 	return `WELCOME TO SHITTY HOROSCOPES LIMITED:
 
 	Hmmmm your sign seems to be: ${randSign}.
 
 	Your Luck is looking: ${randLuck}
 
-	Given that, we advise you to:
-
-	${randAdvice}
-	`
+	Given that, we advise you to: ${randAdvice}`
 }
 
-console.log(horoscope())
+console.log(horoscope());
