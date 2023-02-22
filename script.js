cf4d4a697c25fde34f270f1369798a12432bf5a0
Felix Rodriguez <108102495+ItsFelixRodriguez@users.noreply.github.com>
2023-02-22 16:11:57 +0900

    convert arrays to object

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-// Arrays for days
-const sign = ['Aries','Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capicorn', 'Aquarius', 'Pisces']
 
-const luck = ['Terrible', 'God Awful', 'Unspeakably Bleak', 'Non-existent', 'Nothing to write home about', '... better not to say', '?!?!?!?!?!?!', 'okayish', 'actually not that bad', 'begrudgingly good', 'no', 'high']
 
-const advice = ['run', 'quit your job', "don't look behind your sholder", 'get into minitures', 'definitely buy that thing', 'put it all on black', 'delete your Facebook', 'change your name', 'build an alliance', 'forget', 'open your windows', 'cancel yourself']
 
+let mixed = {
+
+	sign : ['Aries','Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capicorn', 'Aquarius', 'Pisces'],
+	luck : ['Terrible', 'God Awful', 'Unspeakably Bleak', 'Non-existent', 'Nothing to write home about', '... better not to say', '?!?!?!?!?!?!', 'okayish', 'actually not that bad', 'begrudgingly good', 'no', 'high'],
+	advice : ['run', 'quit your job', "don't look behind your sholder", 'get into minitures', 'definitely buy that thing', 'put it all on black', 'delete your Facebook', 'change your name', 'build an alliance', 'forget', 'open your windows', 'cancel yourself']
+}
 
 
 // Random Number Function 
